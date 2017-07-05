de5441d06840fb3afef0817d4cb2612de61a0f29
Jittakorn <alittle.artist@hotmail.com>
2017-07-05 21:59:30 +0700

    initial file

---
+++ js/mml/script.js
@@ -0,0 +1,22 @@
+function pageInvoke() {
+    $("#footer").load("footer.html");
+    $("#header").load("header.html");
+    $("#slider").load("slider.html", function () {
+        // initialize carousel and marquee
+        $(".owl-carousel").owlCarousel({
+            items: 1,
+            smartSpeed: 900
+            /*nav: true*/
+        });
+        $('.marquee-slide').marquee({
+            //speed in milliseconds of the marquee
+            duration: 10000
+        });
+    });
+}
+
+$(document).ready(function () {
+    // Call Template
+    pageInvoke();
+
+});
